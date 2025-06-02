3fd240f3c47773f2c31331c86a43589214d226bb
Ahn-Jwon <ajw0376@gmail.com>
2025-06-02 12:58:15 +0900

    Initial commit

---
+++ SmartShop/SmartShop/SmartShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartShopApp.swift
+//  SmartShop
+//
+//  Created by 안재원 on 5/20/25.
+//
+
+import SwiftUI
+
+@main
+struct SmartShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SmartShop/SmartShopTests/SmartShopTests.swift
@@ -0,0 +1,17 @@
+//
+//  SmartShopTests.swift
+//  SmartShopTests
+//
+//  Created by 안재원 on 5/20/25.
+//
+
+import Testing
+@testable import SmartShop
+
+struct SmartShopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
